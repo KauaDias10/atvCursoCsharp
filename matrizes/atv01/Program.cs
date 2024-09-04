052c3f8ef1994c92f2aab2fb164cbe63423c2ede
Kauã Christian Santos Dias <Kaua Christian@scmpm.local>
2024-09-04 14:48:14 -0300

    Matriz e forEach

---
+++ matrizes/atv01/Program.cs
@@ -0,0 +1,12 @@
+﻿string[] matriz = ["B123", "C234", "A345","C15","B177","G3003","C235","B179"];
+
+foreach (string teste in matriz){
+    if(teste.StartsWith("B")){
+        Console.WriteLine($"essa string começa com B {teste}");
+    }    
+}
+
+
+
+
+
