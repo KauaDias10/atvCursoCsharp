619d600a6eca7bd5e284150003e5b6f408154d62
KauaDias10 <ckaua546@gmail.com>
2024-09-24 17:05:57 -0300

    24/09

--- TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net8.0/TestProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfadfda1ae81a46c03aa20cd8b5015513fee38d7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a07702175367f88d7e5084f21ceb07365c7b382d")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ switch-case/Program.cs
@@ -0,0 +1,56 @@
+﻿// SKU = Unidade de Manutenção de Estoque (Stock Keeping Unit)
+// Formato do valor SKU: <produto #>-<código de 2 letras da cor>-<código do tamanho>
+string sku = "01-MN-L";
+
+string[] produto = sku.Split('-');
+
+string tipo = ""; // type
+string cor = ""; // color
+string tamanho = ""; // size
+
+switch (produto[0])
+{
+    case "01":
+        tipo = "Moletom"; // Sweat shirt
+        break;
+    case "02":
+        tipo = "Camiseta"; // T-Shirt
+        break;
+    case "03":
+        tipo = "Calça de moletom"; // Sweat pants
+        break;
+    default:
+        tipo = "Outro"; // Other
+        break;
+}
+
+switch (produto[1])
+{
+    case "BL":
+        cor = "Preto"; // Black
+        break;
+    case "MN":
+        cor = "Vinho"; // Maroon
+        break;
+    default:
+        cor = "Branco"; // White
+        break;
+}
+
+switch (produto[2])
+{
+    case "S":
+        tamanho = "Pequeno"; // Small
+        break;
+    case "M":
+        tamanho = "Médio"; // Medium
+        break;
+    case "L":
+        tamanho = "Grande"; // Large
+        break;
+    default:
+        tamanho = "Outro"; // Other
+        break;
+}
+
+Console.WriteLine($"Produto: {tamanho} {cor} {tipo}");
